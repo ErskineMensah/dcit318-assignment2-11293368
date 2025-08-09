94ce0d3cea6a21c1ff4f669ded4f9e065c48bd15
Mensah <emensah669@st.ug.edu.gh>
2025-08-09 08:58:22 +0000

    Worked on the last part which has to do with the interfaces

---
+++ Interfaces_ConsoleApp/Interfaces_ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the shape instances
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            // Calling Move() method for each instanace
+            car.Move();
+            bicycle.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
