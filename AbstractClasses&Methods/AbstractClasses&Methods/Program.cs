f259b72ff38599c7d56b046b1951c9d21ba720f7
Mensah <emensah669@st.ug.edu.gh>
2025-08-09 08:36:57 +0000

    Setup and worked on the Abstract Classes and Methods for the various shapes

---
+++ AbstractClasses&Methods/AbstractClasses&Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses_Methods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(20);
+            Shape rectangle = new Rectangle(7, 9);
+
+            Console.WriteLine($"The area of a circle of radius {((Circle)circle).Radius} is {circle.GetArea()}.");
+            Console.WriteLine($"The area of a rectangle of width {((Rectangle)rectangle).Width} and height " +
+            $"{((Rectangle)rectangle).Height} is {rectangle.GetArea()}.");
+        }
+    }
+}
